5cc88342476ca4402294796e357558926e535a31
SoulOfDarknes <ve4nuy@rambler.ru>
2022-10-11 19:00:14 +0300

    footer add

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Map from '../Map/Map'
+
+import '../Form/Form.scss'
+ 
+ function Form () {
+    return (
+      <div className='form__container'>
+            Hello, world
+            <Map />
+      </div>
+    )
+}
+
+export default Form;
